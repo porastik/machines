9fc31db725b177fe9e8ae54607eb5e4e149a8ab2
idea3dsvk <idea3dsvk@gmail.com>
2026-01-10 20:38:53 +0100

    Deploy to GitHub Pages

---
+++ chunk-KA3O3SQA.js
@@ -0,0 +1,1 @@
+import{S as i,Ta as e,fc as n}from"./chunk-YATXNVNU.js";var r=class r{constructor(){this.translationService=i(n)}transform(t){return this.translationService.getTranslation(t)}};r.\u0275fac=function(o){return new(o||r)},r.\u0275pipe=e({name:"translate",type:r,pure:!1});var a=r;export{a};
